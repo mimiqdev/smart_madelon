2c7824c8924c49578ac30eb3473266a9ccdf72ae
Mimikyu <mail@mimikyu.art>
2024-11-18 14:59:54 +0800

    Update

--- custom_components/madelon_ventilation/__init__.py
+++ custom_components/madelon_ventilation/__init__.py
@@ -13,16 +13,15 @@ PLATFORMS: list[Platform] = [Platform.SENSOR, Platform.FAN, Platform.SWITCH]
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     """Set up the Madelon Ventilation component."""
     logging.getLogger(__name__).info("Setting up Madelon Ventilation")
+
+    hass.data.setdefault(DOMAIN, {})
     host = config[DOMAIN].get("host")
-    # hass.async_create_task(async_load_platform(hass, Platform.FAN, DOMAIN, {}, config))
-    # hass.async_create_task(async_load_platform(hass, Platform.SENSOR, DOMAIN, {}, config))
-    # hass.async_create_task(async_load_platform(hass, Platform.SWITCH, DOMAIN, {}, config))
     system = FreshAirSystem(host)
     hass.data[DOMAIN] = {"system": system}
     return True
 
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Set up the Fresh Air System from a config entry."""
-    logger = logging.getLogger(__name__)
+    logging.getLogger(__name__).info("Setting up Madelon Ventilation entry")
     await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
     return True
